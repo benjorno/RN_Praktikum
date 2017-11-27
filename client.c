38f7ea86e92c092bc718ce791c85744eaca6101b
bennet <bennet.honisch@haw-hamburg.de>
2017-11-27 15:27:15 +0100

    changed client

--- client.c
+++ client.c
@@ -2,10 +2,10 @@
 
 #define LOCAL_PORT 15000
 #define LOCAL_IP "141.22.27.106"
-int port = 15000;
 char username[USERNAME_REAL_SIZE];
-
-
+char command[255];
+char username[USERNAME_REAL_SIZE];
+#define MAXBUFLEN 1024
 //initialisiere einen Discovery Request
 void sendDiscoveryRequestTo(char* destinationIp, int destinationPort){
 	int socketRequest;
@@ -175,6 +175,7 @@ void* messageHandlerMain(void * socket_fd_p) {
 				printf("\nNeue Nachricht: %s \n", header.user_message);
 			} else if (header.type == DISCOVERY_REQUEST) {
 				//aktualisiere meine Liste
+				//merken von wem der Request kam und einen Reply erstellen und in die queue einreihen
 			}
 		} else {
 			printf("Der Header enthaelt die unbekannte Versionnummer %d \n", header.version);
@@ -183,9 +184,151 @@ void* messageHandlerMain(void * socket_fd_p) {
 	return NULL;
 }
 
-char command[255];
-char username[USERNAME_REAL_SIZE];
-#define BUF 1024
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+void* messageHandlerMain2() {
+	 fd_set master;    // master file descriptor list
+	    fd_set read_fds;  // temp file descriptor list for select()
+	    int fdmax;        // maximum file descriptor number
+
+	    int listener;     // listening socket descriptor
+	    int newfd;        // newly accept()ed socket descriptor
+	    struct sockaddr_storage remoteaddr; // client address
+	    socklen_t addrlen;
+
+	    char buf[256];    // buffer for client data
+	    int nbytes;
+
+	    char remoteIP[INET6_ADDRSTRLEN];
+
+	    int yes=1;        // for setsockopt() SO_REUSEADDR, below
+	    int i, j, rv;
+
+	    struct addrinfo hints, *ai, *p;
+
+	    FD_ZERO(&master);    // clear the master and temp sets
+	    FD_ZERO(&read_fds);
+
+	    // get us a socket and bind it
+	    memset(&hints, 0, sizeof hints);
+	    hints.ai_family = AF_UNSPEC;
+	    hints.ai_socktype = SOCK_STREAM;
+	    hints.ai_flags = AI_PASSIVE;
+	    if ((rv = getaddrinfo(NULL, LOCAL_PORT, &hints, &ai)) != 0) {
+	        fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+	        exit(1);
+	    }
+
+	    for(p = ai; p != NULL; p = p->ai_next) {
+	        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+	        if (listener < 0) {
+	            continue;
+	        }
+
+	        // lose the pesky "address already in use" error message
+	        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+	        if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
+	            close(listener);
+	            continue;
+	        }
+
+	        break;
+	    }
+
+	    // if we got here, it means we didn't get bound
+	    if (p == NULL) {
+	        fprintf(stderr, "selectserver: failed to bind\n");
+	        exit(2);
+	    }
+
+	    freeaddrinfo(ai); // all done with this
+
+	    // listen
+	    if (listen(listener, 10) == -1) {
+	        perror("listen");
+	        exit(3);
+	    }
+
+	    // add the listener to the master set
+	    FD_SET(listener, &master);
+
+	    // keep track of the biggest file descriptor
+	    fdmax = listener; // so far, it's this one
+
+	    // main loop
+	    for(;;) {
+	        read_fds = master; // copy it
+	        if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+	            perror("select");
+	            exit(4);
+	        }
+
+	        // run through the existing connections looking for data to read
+	        for(i = 0; i <= fdmax; i++) {
+	            if (FD_ISSET(i, &read_fds)) { // we got one!!
+	                if (i == listener) {
+	                    // handle new connections
+	                    addrlen = sizeof remoteaddr;
+	                    newfd = accept(listener,
+	                        (struct sockaddr *)&remoteaddr,
+	                        &addrlen);
+
+	                    if (newfd == -1) {
+	                        perror("accept");
+	                    } else {
+	                        FD_SET(newfd, &master); // add to master set
+	                        if (newfd > fdmax) {    // keep track of the max
+	                            fdmax = newfd;
+	                        }
+	                        printf("selectserver: new connection from %s on "
+	                            "socket %d\n",
+	                            inet_ntop(remoteaddr.ss_family,
+	                                get_in_addr((struct sockaddr*)&remoteaddr),
+	                                remoteIP, INET6_ADDRSTRLEN),
+	                            newfd);
+	                    }
+	                } else {
+	                    // handle data from a client
+	                    if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+	                        // got error or connection closed by client
+	                        if (nbytes == 0) {
+	                            // connection closed
+	                            printf("selectserver: socket %d hung up\n", i);
+	                        } else {
+	                            perror("recv");
+	                        }
+	                        close(i); // bye!
+	                        FD_CLR(i, &master); // remove from master set
+	                    } else {
+	                        // we got some data from a client
+	                        for(j = 0; j <= fdmax; j++) {
+	                            // send to everyone!
+	                            if (FD_ISSET(j, &master)) {
+	                                // except the listener and ourselves
+	                                if (j != listener && j != i) {
+	                                    if (send(j, buf, nbytes, 0) == -1) {
+	                                        perror("send");
+	                                    }
+	                                }
+	                            }
+	                        }
+	                    }
+	                } // END handle data from client
+	            } // END got new incoming connection
+	        } // END looping through file descriptors
+	    } // END for(;;)--and you thought it would never end!
+}
+
+
+
+
 void getUsername() {
     printf("Bitte geben Sie einen Benutzernamen ein: \n");
 	fgets(username, USERNAME_REAL_SIZE, stdin);
@@ -197,12 +340,11 @@ void getIPFromUsername(char username[]) {
 }
 
 int main(int argc, char **argv) {
-	//startLocalServer();
 	getUsername();
 
 	/**
 	 * Starte lokalen "Server" um Nachrichten zu empfangen
-	 */
+
 	struct sockaddr_in sa;
 	int res;
 	int socketLocal;
@@ -212,7 +354,6 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "ERROR: Es kann kein Socket erstellt werden.\n");
 		exit(EXIT_FAILURE);
 	}
-
 	memset(&sa, 0, sizeof(sa)); // Struktur Initialisieren.
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(LOCAL_PORT);
@@ -227,11 +368,19 @@ int main(int argc, char **argv) {
 	if (connect(socketLocal, (struct sockaddr *) &sa, sizeof sa) == -1) {
 		perror("connect failed");
 		close(socketLocal);
-	}
+	}*/
 
 	/**
 	**Thread starten um Nachrichten vom Server zu empfangen
 	*/
+	pthread_t messageHandlerThread2;
+	int messageHandler = pthread_create(&messageHandlerThread2, NULL, messageHandlerMain2, 0);
+	if (messageHandler) {
+		printf("ERROR; return code from pthread_create() is %d\n", messageHandler);
+		//close(socketLocal);
+		exit(-1);
+	}
+	/*
 	pthread_t messageHandlerThread;
 	int* socketLocalServer = &socketLocal;
 	int messageHandler = pthread_create(&messageHandlerThread, NULL, messageHandlerMain, (void*) socketLocalServer);
@@ -240,15 +389,18 @@ int main(int argc, char **argv) {
 		close(socketLocal);
 		exit(-1);
 	}
+	*/
 
-
+	/**
+	 * Benutzereingaben abfragen
+	 */
 	while (1) {
 		printf("Bitte geben sie einen Befehl ein: \n");
 		memset((void  *) &command, 0, 255 * sizeof(char));
 		fgets(command, 255, stdin);
 		command[strcspn(command, "\n")] = 0;
-		if (strcasecmp(command, "/info") == 0) {
-			//infoLoad(SocketFD);
+		if (strcasecmp(command, "/discovery") == 0) {
+			//schicke discovery request
 		} else if (strcasecmp(command, "/exit") == 0) {
 			//userCloseConnection(SocketFD);
 			//logoutflag = LOGOUT;
@@ -262,7 +414,7 @@ int main(int argc, char **argv) {
 			fgets(message, MAX_MESSAGE_SIZE, stdin);
 			username_dest[strcspn(username_dest, "\n")] = 0;
 			getIPFromUsername(username_dest);
-			sendMessageTo(message, port, argv[1], MAX_MESSAGE_SIZE);
+			sendMessageTo(message, LOCAL_PORT, argv[1], MAX_MESSAGE_SIZE);
 		} else {
 			printf("Unbekannter Befehl \n");
 		}
